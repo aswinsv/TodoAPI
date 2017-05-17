dbdec97de42d4340cc727215831155360182cccd
aswinram.92@gmail.com <aswinram.92@gmail.com>
2017-05-17 01:58:22 -0400

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -4,8 +4,49 @@ var app=express();
 
 var port= process.env.PORT || 3000;
 
+var todos=[{
+	id:1,
+	description: 'Complete Cracking the Coding Interview',
+	completed: false
+},{
+	id:2,
+	description: 'Complete Mobile Application',
+	completed: true
+},
+{
+	id:3,
+	description: 'Complete Node js Application',
+	completed: true
+},
+{
+	id:4,
+	description: 'Watch Arsenal Game',
+	completed: false
+}];
+
 app.get('/',function(req,res) {
 	res.send('Todo API Root');
+});
+
+app.get('/todos',function(req,res){
+		res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res){
+	var todoID=req.params.id;
+	var flag=true;
+
+	for(var i=0; i< todos.length;i++)
+	{
+		if(todos[i].id == todoID)
+		{
+			res.json(todos[i]);
+			flag=false;
+		}	
+	}	
+
+	if(flag)
+		res.status(404).send();
 });
 
 app.listen(port,function() {
