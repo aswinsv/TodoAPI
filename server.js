74020c67eecbad4a7c196421bb0e4ce44da4a60a
aswinram.92@gmail.com <aswinram.92@gmail.com>
2017-05-28 12:24:09 -0400

    Update id

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/todos',function(req,res){
 		res.json(todos);
 });
 
+// display the input id from todos array
 app.get('/todos/:id',function(req,res){
 	var todoID=parseInt(req.params.id,10);
 
@@ -37,6 +38,8 @@ app.get('/todos/:id',function(req,res){
 	
 
 });
+
+// delete id from todos
 
 app.delete('/todos/:id',function(req,res){
 
@@ -53,6 +56,51 @@ app.delete('/todos/:id',function(req,res){
 
 });
 
+//Update todo Array
+
+app.put('/todos/:id',function(req,res){
+	var todoID=parseInt(req.params.id,10);
+
+	var matchedTodo=_.findWhere(todos,{id:todoID});
+
+	var body=_.pick(req.body,'description','completed');
+
+
+	var validAttributes={};
+
+
+	if(!matchedTodo)
+	{
+			res.status(404).json({"error":"no todo found with that id"});
+	}	
+
+
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+	{ 
+		validAttributes.completed=body.completed;
+	}	
+	else if(body.hasOwnProperty('completed'))
+	{
+		return res.status(400).send();
+	}	
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length != 0)
+	{
+		validAttributes.description=body.description;
+	}	
+	else if(body.hasOwnProperty('description'))
+	{
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo,validAttributes);	
+
+	res.json(matchedTodo);
+});
+
+
+// POST-Sends value to server
 app.post('/todos',function(req,res){
 	var body=_.pick(req.body,'description','completed');
 
